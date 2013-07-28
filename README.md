957df4b865136fadf323053fad5e71bdbed7183d
paullang <paul.andrew.lang@gmail.com>
2013-07-27 22:38:18 -0700

    v0.0.2 add defaultFrom and improve README

--- README.md
+++ README.md
@@ -1,9 +1,69 @@
 # hapi-mail
 
-Email plugin for Hapi.js
+Email plugin for [Hapi.js](https://github.com/spumko/hapi)
+
+## Background
+
+Currently this plugin supports handlebars as the HTML template engine for the email body and Amazon Simple Email Service (SES) as the email engine, but it was designed in a way that more providers can be easily added if necessary.
 
 ## Getting Started
-Install **hapi-mail** by either running `npm install hapi-mail --save` in your sites working directory or add 'hapi-mail' to the dependencies section of the 'package.json' file and run npm install.
+Install **hapi-mail** by either running `npm install hapi-mail --save` in your application's root directory or add 'hapi-mail' to the dependencies section of the 'package.json' file and run npm install.
 
-### How to use
-See test/plugin.js
+## How to use
+
+This module assumes you are already familiar with [Hapi.js](https://github.com/spumko/hapi) and it's plugin conventions.
+
+```javascript 
+var options = { 
+    template: {
+        engine: 'handlebars',
+        path: './emails/' 
+    },
+    email: {
+        engine: 'ses',
+        options: { accessKeyId: process.env.AWS_BACKEND_ID, secretAccessKey: process.env.AWS_BACKEND_SECRET, region: 'us-east-1' },
+        defaultFrom: 'you@domain.com'
+    }
+};
+
+var email = {
+    from: 'somebody@yourdomain.com',
+    to: ['user@userland.com'],
+    cc: ['boss@yourdomain.com'],
+    bcc: ['somebody@yourdomain.com'],
+    replyTo: ['reply@yourdomain.com'],
+    returnPath: 'bounce@yourdomain.com',
+    subject: 'Example registration confirmation',
+    bodyTemplate: 'registration.html',
+    bodyData: { name: 'Paul Lang', username: 'paullang' }
+};
+
+server.pack.allow({ }).require('hapi-mail', options, function (err) {
+
+   ....
+});
+
+
+// If you have a reference to the server, you can use this
+server.plugins['hapi-mail'].sendMail(email, function(err, response) {
+
+   ....
+});
+
+// If you have a reference to a request, you can use this
+request.server.plugins['hapi-mail'].sendMail(email, function(err, data) {
+
+   ....
+});
+
+
+```
+
+**hapi-mail PROTIPS:** 
+See [test/plugin.js](https://github.com/paullang/hapi-mail/tree/master/test) for working example, but you will need to set three environment variables for it to run: AWS_BACKEND_ID, AWS_BACKEND_SECRET, EMAIL
+
+Since this module name has a - in the name, you cannot use dot notation to register and access it.
+e.g. server.plugins['hapi-mail'].sendMail(...) and cannot use server.plugins.hapi-mail.sendMail(...)
+
+**AWS PROTIP:** Don't store your actual AWS key and secret in your source code and don't use your root AWS key and secret for your applications.
+Setup a limited access key using [AWS IAM](http://aws.amazon.com/iam/) and either put them in environment variables or into a separate configuration file that won't get uploaded somewhere public like Github or NPM.
--- test/plugin.js
+++ test/plugin.js
@@ -15,18 +15,35 @@ var it = Lab.test;
 
 describe('hapi-mail', function () {
 
-    it('can be added as a plugin to hapi', function (done) {
+    var baseOptions = { 
+        template: {
+            engine: 'handlebars',
+            path: './test/emails/' 
+        },
+        email: {
+            engine: 'ses',
+            options: { accessKeyId: process.env.AWS_BACKEND_ID, secretAccessKey: process.env.AWS_BACKEND_SECRET, region: 'us-east-1' }
+        }
+    };
 
-    	var options = { 
-            template: {
-                engine: 'handlebars',
-                path: './test/emails/' 
-            },
-            email: {
-                engine: 'ses',
-                options: { accessKeyId: process.env.AWS_BACKEND_ID, secretAccessKey: process.env.AWS_BACKEND_SECRET, region: 'us-east-1' }
-            }
-        };
+    var testEmail = process.env.EMAIL;
+
+    var baseEmail = {
+        from: testEmail,
+        to: [testEmail],
+        cc: [],
+        bcc: [],
+        replyTo: [testEmail],
+        returnPath: testEmail,
+        subject: 'Example registration confirmation',
+        bodyTemplate: 'registration.html',
+        bodyData: { name: 'Paul Lang', username: 'paullang' }
+    };
+
+    it('can be added as a plugin to hapi and send email', function (done) {
+
+        var options = Hapi.utils.clone(baseOptions);
+        var email = Hapi.utils.clone(baseEmail);
 
         var server = new Hapi.Server();
         server.pack.allow({ }).require('../', options, function (err) {
@@ -34,26 +51,34 @@ describe('hapi-mail', function () {
             expect(err).to.not.exist;
         });
 
-        var testEmail = 'paul.andrew.lang@gmail.com';
-        var email = {
-            from: testEmail,
-            to: [testEmail],
-            cc: [],
-            bcc: [],
-            replyTo: [testEmail],
-            returnPath: testEmail,
-            subject: 'Example registration confirmation',
-            bodyTemplate: 'registration.html',
-            bodyData: { name: 'Paul Lang', username: 'paullang' }
-        }
+        server.plugins['hapi-mail'].sendMail(email, function(err, response) {
+
+            expect(err).to.not.exist;
+            expect(response.MessageId).to.not.equal('');
+            done();
+        });
+    });
+
+    it('can send an email from defaultFrom', function (done) {
+        var options = Hapi.utils.clone(baseOptions);
+        options.email.defaultFrom = testEmail;
+
+        var email = Hapi.utils.clone(baseEmail);
+        delete email.from;
+        email.subject += ' from defaultFrom';
+
+        var server = new Hapi.Server();
+        server.pack.allow({ }).require('../', options, function (err) {
+
+            expect(err).to.not.exist;
+        });
 
         server.plugins['hapi-mail'].sendMail(email, function(err, response) {
 
             expect(err).to.not.exist;
-
             expect(response.MessageId).to.not.equal('');
-
             done();
         });
     });
+
 });
